1e86d42e08f31470d9a9c2a90f9d6575e1a65f17
NM6293 <B00127049@mytudublin.ie>
2020-11-29 17:26:41 +0000

    Level1Done

---
+++ NatashaM_PersonalProject/Assets/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGameActive;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI ResetText;
+    public TextMeshProUGUI WONText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+    }
+
+    //when you hit your enemy method
+    public void GameOver()
+    {
+        isGameActive = false;
+        ResetText.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+
+
+    }
+    public void GameWon()
+    {
+        isGameActive = false;
+        WONText.gameObject.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //restart
+        if (Input.GetKeyDown("r"))
+        { //If you press R
+            SceneManager.LoadScene("SampleScene"); //Load scene called Game
+        }
+    }
+}
---
+++ NatashaM_PersonalProject/Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private float turnSpeed = 10.0f;
+    private float horizontalinput;
+    private float forwardInput;
+    public AudioClip Crash;
+    public AudioClip lose;
+    private AudioSource playerAudio;
+    private GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerAudio = GetComponent<AudioSource>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // dont go off screen
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        //turn on input
+        horizontalinput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalinput);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if you hit the farmer
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+
+            SceneManager.LoadScene("Level2");
+            playerAudio.PlayOneShot(Crash, 1.0f);
+        }
+        //if you hit an enemy
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            gameManager.GameOver();
+            Debug.Log("You lost :(");
+            playerAudio.PlayOneShot(lose, 1.0f);
+        }
+
+
+    }
+}
+
+//sounds asset
+
+/* https://assetstore.unity.com/packages/audio/sound-fx/free-casual-game-sfx-pack-54116 */
+
+
